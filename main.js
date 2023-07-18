463e9c1b5fa3eb79cfd4d889b749d42d96ff322b
JurayevAkmal <jurayevakmal0307@gmail.com>
2023-07-18 19:04:17 +0500

    dom

---
+++ main.js
@@ -0,0 +1,167 @@
+// let map = new Map();
+// map.set("1", "str1");   
+// map.set(1, "num1");      
+// map.set(true, "bool1"); 
+// console.log(map.get(1));
+// console.log(map.get("1"));
+// console.log(map.size);
+
+
+
+// let map = new Map([[1, "z"],[234, "as"]]);
+// map.set("123", "string", "hello world", 'js')
+// console.log(map.has(234));
+
+
+// let mapIn = new Map();
+// mapIn.set(2,'12')
+// mapIn.set(4,'56')
+// mapIn.set(6,'hello')
+// mapIn.set(7,'455')
+// console.log(mapIn.has(2));
+
+
+// let animal = new Map()
+// animal.set(2,'lion')
+// animal.set(4,'tiger')
+// animal.set(6,'bear')
+// console.log(animal.get(2));
+
+
+// const myMap = new Map([[1, "Tom"], [2, "Bob"], [3, "Sam"]]);
+ 
+// myMap.forEach(function(value1, value2, map){
+//     console.log(value2, value1);
+// })
+
+
+
+// let a = [ 3, 5,6 ]
+// let twice = a.map(function(a)
+// {
+//     return a*2
+// })
+// console.log(twice);
+
+
+// let a = [4,7,9]
+// a[9] = 5;
+// let c = a.map(function(a){
+//     return a**2
+// })
+// let d = new Map();
+// d.set(c)
+// console.log(d.has(16));
+// console.log(d);
+
+
+
+
+// const numbers = [65, 44, 12, 4];
+// const newArr = numbers.map(myFunction)
+
+// function myFunction(num) {
+//   return num * 10;
+// }
+
+
+// let num = [12, 34, 67]
+// let numF = num.map(myFunc)
+// let a =  myFunc(a) => return a * 10 ;
+
+// myFunc()
+// console.log(s);
+
+
+
+
+// let num = [12, 34, 67];
+
+// let myFunc = (a) => {
+//     return a * 10;
+// };
+
+// let numF = num.map(myFunc);
+// console.log(numF); 
+
+
+
+
+
+
+// let mapp = new Map();
+// mapp.set(122,'assad')
+// mapp.set(13,'asdfvad')
+// mapp.set(43,'asrg')
+// mapp.set(43,'argd')
+// mapp.delete(13)
+// console.log(mapp);
+
+
+
+// let mapp = new Map();
+// mapp.set(122,'assad')
+// mapp.set(13,'asdfvad')
+// mapp.set(43,'asrg')
+// mapp.set(43,'argd')
+// mapp.clear();
+// console.log(mapp.size);
+
+
+
+// let words = ['heey', 'hello', 'world'];
+// let doubledWords = words.map (word => {
+//     return word.split("").map(char => {
+//       return char + char;
+//     }).join("");
+//   });
+//   console.log(doubledWords); 
+
+
+// let numbers = [1, 2, 3, 4, 5];
+// let doubleNumbers = numbers.map(num => num * 2);
+// console.log(doubleNumbers);
+
+
+// function doubleStringLength(strings) {
+//     return strings.map(function(str) {
+//       return str.length * 2;
+//     });
+//   }
+  
+//   let strings = ["hello", "world", "foo", "bar"];
+//   let doubledStringLengths = doubleStringLength(strings);
+//   console.log(doubledStringLengths);
+
+
+// let myDiv = document.getElementById("staf");
+// myDiv.style.backgroundColor = "red";
+
+
+
+// let myDiv = document.getElementById("staf");
+// myDiv.style.border = "5px solid red";
+
+
+// let myDiv = document.getElementById("staf");
+// myDiv.style.border = "1px solid black";
+// myDiv.style.backgroundImage = "url(https://picsum.photos/190/190)";
+// myDiv.style.margin = "border box";
+
+
+// let myDiv = document.getElementById("staf");
+// myDiv.style.width = "500px";
+// myDiv.style.margin = "0 auto";
+// myDiv.style.border = "1px solid black";
+// myDiv.style.borderRadius = "20px";
+
+
+
+
+let ha1 = document.getElementById("lorem");
+// ha1.style.fontSize = "123px";
+
+// ha1.style.fontFamily = ""
+ ha1.style.width="100px";
+ ha1.style.textAlign="center";
+ let ha2 = document.body.style.textAlign = "center";
